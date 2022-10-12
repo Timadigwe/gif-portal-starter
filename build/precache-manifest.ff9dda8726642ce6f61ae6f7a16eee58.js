97d066f8f39d2d2c9c02baf6b7cc719ecf54e876
timadigwe <timothyadigwe2019@gmail.com>
2022-10-12 20:39:27 +0100

    build file created

---
+++ build/precache-manifest.ff9dda8726642ce6f61ae6f7a16eee58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea0d3ef66938ba152ba5",
+    "url": "/static/css/main.f75967cf.chunk.css"
+  },
+  {
+    "revision": "ea0d3ef66938ba152ba5",
+    "url": "/static/js/main.1aaf6b5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "11a693a2432ca6649c66",
+    "url": "/static/js/2.874a26ee.chunk.js"
+  },
+  {
+    "revision": "5c97ee2a3ed91e9de148f8d40b2d324e",
+    "url": "/static/media/twitter-logo.5c97ee2a.svg"
+  },
+  {
+    "revision": "09fdaa4e32875beea0534dd9190c6778",
+    "url": "/index.html"
+  }
+];
